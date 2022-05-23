d3a579e9b28e1ab5fc4785d45a4d9614a129ab67
Naresh Bhosale <nareshbhosale73@gmail.com>
2022-05-23 08:58:36 +0530

    commit

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ const connectDatabase = () => {
 
     mongoose.connect(process.env.DBURI).then((data) => {
         console.log(`Mongodb connected with server: ${data.connection.host}`);
-    });
+    }).catch((e) => console.log(e));
 };
 
 module.exports = connectDatabase;
